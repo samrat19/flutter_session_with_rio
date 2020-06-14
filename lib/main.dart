669ae6ac683bcaf24b0838d8095622995d0e467d
Atri Das <babinsamrat123@gmail.com>
2020-06-14 21:42:46 +0530

    adds hero animation

--- lib/main.dart
+++ lib/main.dart
@@ -6,24 +6,71 @@ main() => runApp(
       ),
     );
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+  void nextPage() {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (BuildContext context) => DetailsScreen(),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.teal,
-          title: Text('Image'),
-        ),
-        body: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Container(
-            height: 400.0,
-            width: 400.0,
-            child: Image.asset(
-              'asset/android_logo.jpg',
-              fit: BoxFit.cover,
+      appBar: AppBar(
+        backgroundColor: Colors.teal,
+        title: Text('Image'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Center(
+          child: GestureDetector(
+            onTap: nextPage,
+            child: Container(
+              height: 100.0,
+              width: 100.0,
+              child: Hero(
+                tag: 'android11',
+                child: Image.asset(
+                  'asset/android_logo.jpg',
+                  fit: BoxFit.cover,
+                ),
+              ),
             ),
           ),
-        ));
+        ),
+      ),
+    );
+  }
+}
+
+class DetailsScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Center(
+          child: Container(
+            height: 300.0,
+            width: 300.0,
+            child: Hero(
+              tag: 'android11',
+              child: Image.asset(
+                'asset/android_logo.jpg',
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
