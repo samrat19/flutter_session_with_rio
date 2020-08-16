875ffec3c84074f2e1f343c3fb9e8da60e3e79a9
Atri Das <babinsamrat123@gmail.com>
2020-08-16 23:56:03 +0530

    uploads stream

--- lib/main.dart
+++ lib/main.dart
@@ -1,37 +1,69 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
-import 'logic/model/news_model.dart';
-import 'logic/news_bloc.dart';
-
-main() => runApp(
-  MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: MyApp(),
-  ),
-);
-
-class MyApp extends StatelessWidget{
-  @override
-  Widget build(BuildContext context) => FutureProvider(
-    create: (BuildContext context) => NewsBloc().loadNewsData(),
-    child: BusinessNewsScreen(),
-  );
+void main() {
+  runApp(MyApp());
 }
 
-class BusinessNewsScreen extends StatelessWidget {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    var news = Provider.of<List<NewsModel>>(context);
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Stream Builder'),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+
+  final String title;
+
+  MyHomePage({Key key, @required this.title}) : super(key: key);
+
+  final MyCounter _myCounter = MyCounter();
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: ListView.builder(
-        itemCount: news.length,
-        itemBuilder: (_,int index) => Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Image.network(news[index].urlToImage),
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: Center(
+        child: StreamBuilder<int>(
+          stream: _myCounter.getCounter(),
+          builder: (context, snapshot) => Text(
+              snapshot.data == null ? '0':snapshot.data.toString(),
+              style: TextStyle(
+                fontSize: 70.0,
+              ),
+            ),
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: ()=>_myCounter.increment(),
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
       ),
     );
   }
+}
+
+class MyCounter{
+  int _counter = 0;
+  StreamController _streamController = StreamController<int>();
+
+  Stream<int> getCounter() => _streamController.stream;
+
+  void increment(){
+    _counter++;
+    _streamController.add(_counter);
+  }
+
+  dispose() => _streamController.close();
 }
